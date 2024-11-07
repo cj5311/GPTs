21da0d6afee934f5ebe69ed3c3b8f6e9cc76cadf
Cheongjo <103722439+cj5311@users.noreply.github.com>
2024-11-07 11:09:16 +0900

    quize edit

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -30,172 +30,173 @@ def format_docs(docs):
 
 api_key = st.session_state.get("api_key", None)
 
-llm = ChatOpenAI(
-        temperature=0.1,
-        model="gpt-4o-mini",
-        api_key=api_key,
-        streaming=True,
-        callbacks=[StreamingStdOutCallbackHandler()]
+if st.session_state.api_key_check : 
+    llm = ChatOpenAI(
+            temperature=0.1,
+            model="gpt-4o-mini",
+            api_key=api_key,
+            streaming=True,
+            callbacks=[StreamingStdOutCallbackHandler()]
+        )
+        
+    question_prompt = ChatPromptTemplate.from_messages(
+        [("system", """
+            
+            Youare a helpful assistant that is role playing as a teacher.
+            
+            Based ONLY on the following context make 10 questions to test the user's Knowledge about the text.
+            Each question should have 4 answers, three of them must be incorrect and one should be correct.
+            
+            Use (o) to signal the correct answer.
+            
+            Question examples : 
+            
+            Question : What is the color of the ocean?
+            Answers : Red| Yellow| Green| Blue(o)
+            
+            Question : what is the capital or Georgia?
+            Answers : Baku| Tbilisi(o)| Manila, Beirut
+            
+            Question : When was Avatar released?
+            Answers :  2007| 2001| 2009(o)| 1998
+            
+            Question : Who was Julius Caesar?
+            Answers : A Roman Emperor(o)| Painter| Actor| Model
+            
+            Your Turn!
+            
+            Context : {context}
+            
+            """)
+            
+            
+        ]
+        
     )
-    
-question_prompt = ChatPromptTemplate.from_messages(
-    [("system", """
-        
-        Youare a helpful assistant that is role playing as a teacher.
-        
-        Based ONLY on the following context make 10 questions to test the user's Knowledge about the text.
-        Each question should have 4 answers, three of them must be incorrect and one should be correct.
-        
-        Use (o) to signal the correct answer.
-        
-        Question examples : 
-        
-        Question : What is the color of the ocean?
-        Answers : Red| Yellow| Green| Blue(o)
-        
-        Question : what is the capital or Georgia?
-        Answers : Baku| Tbilisi(o)| Manila, Beirut
-        
-        Question : When was Avatar released?
-        Answers :  2007| 2001| 2009(o)| 1998
-        
-        Question : Who was Julius Caesar?
-        Answers : A Roman Emperor(o)| Painter| Actor| Model
-        
-        Your Turn!
-        
-        Context : {context}
-        
-        """)
-        
-        
-    ]
-    
-)
 
-question_chains = {"context":format_docs}| question_prompt | llm
+    question_chains = {"context":format_docs}| question_prompt | llm
 
-formatting_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system","""
-         You are a powerful formatting algorithm,
-         
-         You format exam questions into JSON format.
-         Answers with (o) are the correct ones.
-         
-         Example Input:
-         
-         Question : What is the color of the ocean?
-         Answers: Red | Yellow | Green | Blue (o)
-         
-         Question : what is the capital or Georgia?
-         Answers : Baku | Tbilisi(o) | Manila, Beirut
+    formatting_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system","""
+            You are a powerful formatting algorithm,
+            
+            You format exam questions into JSON format.
+            Answers with (o) are the correct ones.
+            
+            Example Input:
+            
+            Question : What is the color of the ocean?
+            Answers: Red | Yellow | Green | Blue (o)
+            
+            Question : what is the capital or Georgia?
+            Answers : Baku | Tbilisi(o) | Manila, Beirut
+            
+            Question : When was Avatar released?
+            Answers :  2007 | 2001 | 2009(o)| 1998
+            
+            Question : Who was Julius Caesar?
+            Answers : A Roman Emperor(o) | Painter | Actor | Model
+            
+            Example Output : 
+            
+            ```
+            json
+            {{ "question":[
+                    {{
+                        "question": "what is the color of the ocean?",
+                        "answers":[
+                            {{
+                                "answer":"Red",
+                                "correct":false
+                            }},
+                            {{
+                                "answer":"Yellow",
+                                "correct":false
+                            }},
+                            {{
+                                "answer":"Grean",
+                                "correct":false
+                            }},
+                            {{
+                                "answer":"Blue",
+                                "correct":true
+                            }},
+                        ]
+                    }},
+                    
+                    {{
+                        "question": "what is the capital or Georgia?",
+                        "answers":[
+                            {{
+                                "answer":"Baku",
+                                "correct":false
+                            }},
+                            {{
+                                "answer":"Tbilisi",
+                                "correct":true
+                            }},
+                            {{
+                                "answer":"Manila",
+                                "correct":false
+                            }},
+                            {{
+                                "answer":"Beirut",
+                                "correct":True
+                            }},
+                        ]
+                    }},
+                    {{
+                        "question": "Who was Julius Caesar?",
+                        "answers":[
+                            {{
+                                "answer":"2007",
+                                "correct":false
+                            }},
+                            {{
+                                "answer":"2001",
+                                "correct":false
+                            }},
+                            {{
+                                "answer":"2009",
+                                "correct":true
+                            }},
+                            {{
+                                "answer":"1998",
+                                "correct":false
+                            }},
+                        ]
+                    }},
+                    {{
+                        "question": "When was Avatar released?",
+                        "answers":[
+                            {{
+                                "answer":" A Roman Emperor",
+                                "correct":true
+                            }},
+                            {{
+                                "answer":"Painter",
+                                "correct":false
+                            }},
+                            {{
+                                "answer":"Actor",
+                                "correct":false
+                            }},
+                            {{
+                                "answer":"Model",
+                                "correct":false
+                            }},
+                        ]
+                    }}
+                    
+                ]
+            }}
+            ```
+            
+            """)
+        ])
         
-         Question : When was Avatar released?
-         Answers :  2007 | 2001 | 2009(o)| 1998
-        
-         Question : Who was Julius Caesar?
-         Answers : A Roman Emperor(o) | Painter | Actor | Model
-         
-         Example Output : 
-         
-         ```
-         json
-         {{ "question":[
-                 {{
-                     "question": "what is the color of the ocean?",
-                     "answers":[
-                         {{
-                             "answer":"Red",
-                             "correct":false
-                         }},
-                         {{
-                             "answer":"Yellow",
-                             "correct":false
-                         }},
-                         {{
-                             "answer":"Grean",
-                             "correct":false
-                         }},
-                         {{
-                             "answer":"Blue",
-                             "correct":true
-                         }},
-                     ]
-                 }},
-                 
-                 {{
-                     "question": "what is the capital or Georgia?",
-                     "answers":[
-                         {{
-                             "answer":"Baku",
-                             "correct":false
-                         }},
-                         {{
-                             "answer":"Tbilisi",
-                             "correct":true
-                         }},
-                         {{
-                             "answer":"Manila",
-                             "correct":false
-                         }},
-                         {{
-                             "answer":"Beirut",
-                             "correct":True
-                         }},
-                     ]
-                 }},
-                 {{
-                     "question": "Who was Julius Caesar?",
-                     "answers":[
-                         {{
-                             "answer":"2007",
-                             "correct":false
-                         }},
-                         {{
-                             "answer":"2001",
-                             "correct":false
-                         }},
-                         {{
-                             "answer":"2009",
-                             "correct":true
-                         }},
-                         {{
-                             "answer":"1998",
-                             "correct":false
-                         }},
-                     ]
-                 }},
-                 {{
-                     "question": "When was Avatar released?",
-                     "answers":[
-                         {{
-                             "answer":" A Roman Emperor",
-                             "correct":true
-                         }},
-                         {{
-                             "answer":"Painter",
-                             "correct":false
-                         }},
-                         {{
-                             "answer":"Actor",
-                             "correct":false
-                         }},
-                         {{
-                             "answer":"Model",
-                             "correct":false
-                         }},
-                     ]
-                 }}
-                 
-             ]
-         }}
-         ```
-         
-         """)
-    ])
-    
-formatting_chain = formatting_prompt | llm
+    formatting_chain = formatting_prompt | llm
 
 
 @st.cache_data(show_spinner="Making Quiz...")
